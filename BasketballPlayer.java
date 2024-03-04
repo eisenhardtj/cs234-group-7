b40e679eca074d8393945ae19a9d522a22634063
Cole Aydelotte <aydelottec@moravian.edu>
2024-03-04 01:59:31 -0500

    Added the the graduation year, height, and weight to the inputPanel.java, and BasketballPlayer.java files.

--- BasketballPlayer.java
+++ BasketballPlayer.java
@@ -2,14 +2,45 @@ public class BasketballPlayer {
     private String name;
     private int number;
     private String position;
+    private int expectedGraduationDate;
+    private double height;
+    private double weight;
 
-    public BasketballPlayer(String name, int number, String position) {
+    public BasketballPlayer(String name, int number, String position, int expectedGraduationDate, double height, double weight) {
         this.name = name;
         this.number = number;
         this.position = position;
+        this.expectedGraduationDate = expectedGraduationDate;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getExpectedGraduationDate() {
+        return expectedGraduationDate;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
     }
 
     public String toString() {
-        return "Name: " + name + ", Number: " + number + ", Position: " + position;
+        return "Name: " + name + ", Number: " + number + ", Position: " + position +
+               ", Graduation Date: " + expectedGraduationDate + ", Height: " + height + ", Weight: " + weight;
     }
 }
--- InputPanel.java
+++ InputPanel.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 public class InputPanel extends JPanel implements ActionListener {
     private Roster roster;
-    private JTextField nameField, numberField, positionField;
+    private JTextField nameField, numberField, positionField, gradField, heightField, weightField;
 
     public InputPanel(Roster roster) {
         this.roster = roster;
@@ -20,6 +20,15 @@ public class InputPanel extends JPanel implements ActionListener {
         add(new JLabel("Position:"));
         positionField = new JTextField();
         add(positionField);
+        gradField = new JTextField();
+        add(new JLabel("Expected graduation date:"));
+        add(gradField);
+        heightField = new JTextField();
+        add(new JLabel("Height:"));
+        add(heightField);
+        weightField = new JTextField();
+        add(new JLabel("Weight:"));
+        add(weightField);
 
         JButton addButton = new JButton("Add Player");
         addButton.addActionListener(this);
@@ -36,11 +45,18 @@ public class InputPanel extends JPanel implements ActionListener {
             String name = nameField.getText();
             int number = Integer.parseInt(numberField.getText());
             String position = positionField.getText();
-            BasketballPlayer player = new BasketballPlayer(name, number, position);
+            int year = Integer.parseInt(gradField.getText());
+            Double height = Double.parseDouble(heightField.getText());
+            int weight = Integer.parseInt(weightField.getText());
+
+            BasketballPlayer player = new BasketballPlayer(name, number, position, year, height, weight);
             roster.addPlayer(player);
         } else if (e.getActionCommand().equals("Remove Player")) {
+            String name = nameField.getText();
+            
             // Implement removing player functionality
             // You can use roster.removePlayer(index) method
         }
     }
 }
+
