cc0b4433762d89305ca06d6ef25db6f79aebbf7b
Cole Aydelotte <aydelottec@moravian.edu>
2024-03-26 12:12:52 -0400

    Fixed Dumpfile.sql
    Fixed Dumpfile.sql

---
+++ dumpfiles/dumpfile.sql
@@ -0,0 +1,161 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for macos14.2 (arm64)
+--
+-- Host: localhost    Database: moravianwomensteam24
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ArchivePlayer`
+--
+
+DROP TABLE IF EXISTS `ArchivePlayer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ArchivePlayer` (
+  `player_id` int NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `player_number` int DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `expected_graduation_date` int DEFAULT NULL,
+  PRIMARY KEY (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ArchivePlayer`
+--
+
+LOCK TABLES `ArchivePlayer` WRITE;
+/*!40000 ALTER TABLE `ArchivePlayer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ArchivePlayer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `archiveplayers`
+--
+
+DROP TABLE IF EXISTS `archiveplayers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `archiveplayers` (
+  `player_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `player_number` int DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `expected_graduation_date` int DEFAULT NULL,
+  PRIMARY KEY (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `archiveplayers`
+--
+
+LOCK TABLES `archiveplayers` WRITE;
+/*!40000 ALTER TABLE `archiveplayers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `archiveplayers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `freethrows`
+--
+
+DROP TABLE IF EXISTS `freethrows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `freethrows` (
+  `date` date DEFAULT NULL,
+  `made` int DEFAULT NULL,
+  `attempted` int DEFAULT NULL,
+  `player_id` int DEFAULT NULL,
+  KEY `player_id` (`player_id`),
+  CONSTRAINT `freethrows_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `teamroster` (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `freethrows`
+--
+
+LOCK TABLES `freethrows` WRITE;
+/*!40000 ALTER TABLE `freethrows` DISABLE KEYS */;
+/*!40000 ALTER TABLE `freethrows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teamroster`
+--
+
+DROP TABLE IF EXISTS `teamroster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teamroster` (
+  `player_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `player_number` int DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `expected_graduation_date` int DEFAULT NULL,
+  PRIMARY KEY (`player_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teamroster`
+--
+
+LOCK TABLES `teamroster` WRITE;
+/*!40000 ALTER TABLE `teamroster` DISABLE KEYS */;
+INSERT INTO `teamroster` VALUES (1,'Cole','Aydelotte',7,'C',2026),(2,'Cole','Ayd',7,'C',2026),(3,'Brody','Ayd',7,'C',2026);
+/*!40000 ALTER TABLE `teamroster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `threepointshots`
+--
+
+DROP TABLE IF EXISTS `threepointshots`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `threepointshots` (
+  `date` date DEFAULT NULL,
+  `made` int DEFAULT NULL,
+  `attempted` int DEFAULT NULL,
+  `player_id` int DEFAULT NULL,
+  KEY `player_id` (`player_id`),
+  CONSTRAINT `threepointshots_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `teamroster` (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `threepointshots`
+--
+
+LOCK TABLES `threepointshots` WRITE;
+/*!40000 ALTER TABLE `threepointshots` DISABLE KEYS */;
+/*!40000 ALTER TABLE `threepointshots` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-26 12:10:50
